2804639f691133bd4f3852f07c216385fcd97503
Adrian <adrianribeiro270@gmail.com>
2025-06-30 19:54:56 -0300

    Correção de bug github

---
+++ clinicateste/src/main/java/br/edu/imepac/clinica/medica/funcionarios/EditarFuncionarios.java
@@ -0,0 +1,23 @@
+package br.edu.imepac.clinica.medica.funcionarios;
+
+import br.edu.imepac.clinica.medica.daos.FuncionariosDao;
+import br.edu.imepac.clinica.medica.entidades.Funcionarios;
+import br.edu.imepac.clinica.medica.outros.Estilo;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.text.JTextComponent;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.util.List;
+
+public class EditarFuncionarios extends JFrame {
+
+
+
+    private JButton btnEditar;
+    private JButton btnFechar;
+}
